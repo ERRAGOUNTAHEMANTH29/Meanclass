d824e142a9dfabe1506dfafd26e888c26cb60d7b
ERRAGOUNTAHEMANTH <hemantherragounta29@gmail.com>
2024-05-23 10:42:06 +0530

    updated

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express()
+const db = require('./sample_node/src/database/db')
+
+db.on("open",()=>{
+app.listen(3000,()=>{
+    console.log("server is running on port 3000")
+})
+})
+db.on("error",()=>{
+console.log("error while connecting to database")
+})
+
+
+
+
+
+
+
+// const http = require("http");
+// const os = require("os");
+// const fs = require("fs");
+
+// // os module
+
+// console.log(os.hostname());
+// console.log(os.type());
+
+// fs.mkdir("demo1", (err) => {
+//   if (err) {
+//     console.log("error", err);
+//   } else {
+//     console.log("created successfully");
+//   }
+// });
+
+// fs.rmdir("demo1", (err) => {
+//   if (err) {
+//     console.log("error while removing the folder", err);
+//   } else {
+//     console.log("removed successfully");
+//   }
+// });
+
+// fs.readFile("demoContent.txt",(err,data)=>{
+//     if (err) {
+//         console.log("error while readfile the folder", err);
+//       } else {
+//         console.log(data.toString(),"data read successfully");
+//       }
+// })
+
+// http
+//   .createServer(function (req, res) {
+//     res.write("server is running on port 3000");
+//     res.end();
+//   })
+//   .listen(3000);
---
+++ sample_node/src/database/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://hemantherragounta29:b22xPqjtHdwZQbfp@cluster0.im0zbw0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+.then(()=>{
+console.log("Mongo db connected")
+})
+.catch((error)=>{
+console.log("Mongo db is not connected",error)
+})
+
+module.exports = mongoose.connection;
